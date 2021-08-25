06747ad88664544c3e583307888d8889c02bdb50
SaqibAarman <saqibahmedaarhan@gmail.com>
2021-08-25 13:51:36 +0530

    fix problem

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const [show, newShow] = useState(false);
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={() => {
+        newShow(true);
+      }}>Show Para</button>
+
+      {show && 
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
